f020ba175d4f554fbcee525f4a637e9031e33675
Inseo Kim <itsinseo@gmail.com>
2024-03-13 17:27:41 +0900

    feat: added new column to GridWithSortFilterApp.vue

--- src/applications/GridWithSortFilterApp.vue
+++ src/applications/GridWithSortFilterApp.vue
@@ -3,12 +3,12 @@ import DemoGrid from '@/components/Grid.vue'
 import { ref } from 'vue'
 
 const searchQuery = ref('')
-const gridColumns = ['name', 'power']
+const gridColumns = ['name', 'power', 'age']
 const gridData = [
-  { name: 'Chuck Norris', power: Infinity },
-  { name: 'Bruce Lee', power: 9000 },
-  { name: 'Jackie Chan', power: 7000 },
-  { name: 'Jet Li', power: 8000 }
+  { name: 'Chuck Norris', power: Infinity, age: 99 },
+  { name: 'Bruce Lee', power: 9000, age: 80 },
+  { name: 'Jackie Chan', power: 7000, age: 40 },
+  { name: 'Jet Li', power: 8000, age: 20 }
 ]
 </script>
 
@@ -16,9 +16,6 @@ const gridData = [
   <form id="search">
     Search <input name="query" v-model="searchQuery">
   </form>
-  <DemoGrid
-    :data="gridData"
-    :columns="gridColumns"
-    :filter-key="searchQuery">
+  <DemoGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
   </DemoGrid>
 </template>
