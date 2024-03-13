eaa77a71f069b82f089d57f8df890851f5b00ecd
Inseo Kim <itsinseo@gmail.com>
2024-03-13 17:00:22 +0900

    refactor: moved application directory

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from '@/applications/FetchingDataApp.vue'
 
 createApp(App).mount('#app')
