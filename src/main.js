dd23b1b01ab81762e57e7c7e94a69f23a6c34f72
Inseo Kim <itsinseo@gmail.com>
2024-03-13 18:37:16 +0900

    feat: tree structured view, children addable

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/GridWithSortFilterApp.vue'
+import App from '@/applications/TreeViewApp.vue'
 
 createApp(App).mount('#app')
