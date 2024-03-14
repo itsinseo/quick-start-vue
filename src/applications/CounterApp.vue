6277b2ef6b05a103036ea277b15f0a3f67f876c4
Inseo Kim <itsinseo@gmail.com>
2024-03-14 18:18:52 +0900

    feat: counter application

---
+++ src/applications/CounterApp.vue
@@ -0,0 +1,20 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#counter
+-->
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  {{ count }}
+  <div>
+    <button @click="count++">Plus 1</button>
+    <button @click="count--">Minus 1</button>
+    <button @click="count*=2">Double</button>
+    <button @click="count/=2">Half</button>
+    <button @click="count=0">Zero</button>
+  </div>
+</template>
