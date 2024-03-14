a4f1db8a63bc9cab324d42d0babd9261aca1dc97
Inseo Kim <itsinseo@gmail.com>
2024-03-14 14:10:37 +0900

    feat: SVG graph application

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/TreeViewApp.vue'
+import App from '@/applications/SvgGraphApp.vue'
 
 createApp(App).mount('#app')
