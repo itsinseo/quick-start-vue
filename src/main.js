d5a5f3686309cb5423c16b0c234ae9e7d22abe75
Inseo Kim <itsinseo@gmail.com>
2024-03-15 18:00:48 +0900

    feat: CRUD application

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/FlightBookerApp.vue'
+import App from '@/applications/CRUDApp.vue'
 
 createApp(App).mount('#app')
