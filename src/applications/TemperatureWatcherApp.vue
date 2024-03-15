16c3d805273160b07a9645cfdfc2d5d678d4575b
Inseo Kim <itsinseo@gmail.com>
2024-03-15 10:04:08 +0900

    feat: temperature converter application
    implemented 2 applications; with event listener/watcher

---
+++ src/applications/TemperatureWatcherApp.vue
@@ -0,0 +1,35 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#temp
+-->
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const c = ref(0)
+const f = ref(32)
+const k = ref(273.15)
+
+watch(c, (newValue) => {
+    f.value = newValue * (9 / 5) + 32
+    k.value = newValue + 273.15
+})
+
+watch(f, (newValue) => {
+    c.value = (newValue - 32) * (5 / 9)
+    k.value = c.value + 273.15
+})
+
+watch(k, (newValue) => {
+    c.value = newValue - 273.15
+    f.value = c.value * (9 / 5) + 32
+})
+
+</script>
+
+<template>
+    <input type="number" v-model="c"> Celsius
+    <br>
+    <input type="number" v-model="f"> Fahrenheit
+    <br>
+    <input type="number" v-model="k"> Kelvin
+</template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/CounterApp.vue'
+import App from '@/applications/TemperatureWatcherApp.vue'
 
 createApp(App).mount('#app')
