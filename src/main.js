9eeba32132d08eef04aee8ed6f73bf58dd955346
Inseo Kim <itsinseo@gmail.com>
2024-03-15 13:54:50 +0900

    feat: flight booker application

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/TodoApp.vue'
+import App from '@/applications/FlightBookerApp.vue'
 
 createApp(App).mount('#app')
