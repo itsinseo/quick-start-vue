26be63e001af27452d9af9998e1c0179aebc5302
Inseo Kim <itsinseo@gmail.com>
2024-03-15 17:13:14 +0900

    feat: timer application
    bug remaining; bug-log.txt

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/FlightBookerApp.vue'
+import App from '@/applications/TimerApp.vue'
 
 createApp(App).mount('#app')
