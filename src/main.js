8f8f1540f615fa6660e1c3f164e631963db9b263
Inseo Kim <itsinseo@gmail.com>
2024-03-18 11:38:31 +0900

    feat: circle drawer application
    simple version using ref, stack-structured history, resizing unable

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from '@/applications/CRUDApp.vue'
+import App from '@/applications/CircleDrawerApp.vue'
 
 createApp(App).mount('#app')
