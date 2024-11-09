022a3cbcc3d302576931c68b884a7e22c0675c5f
Inseo <itsinseo@gmail.com>
2024-11-10 01:25:45 +0900

    feat: QR color inverting

--- src/components/CustomQrScanner.vue
+++ src/components/CustomQrScanner.vue
@@ -7,8 +7,7 @@ const emit = defineEmits(['emitQrScanResult']);
 
 const formatsToSupport = [Html5QrcodeSupportedFormats.QR_CODE];
 const config = {
-  fps: 30,
-  formatsToSupport
+  fps: 30
 };
 const elementName = ref('custom-qr-reader');
 const html5QrScanner = ref();
@@ -26,34 +25,29 @@ const initQrScanner = () => {
   Html5Qrcode.getCameras()
     .then(devices => {
       if (devices && devices.length) {
-        const cameraId = devices[0].id;
-        html5QrScanner.value = new Html5Qrcode(elementName.value, {
-          formatsToSupport
-        });
+        // const cameraId = devices[0].id;
+        html5QrScanner.value = new Html5Qrcode(elementName.value);
         html5QrScanner.value.start(
-          cameraId,
+          { facingMode: 'environment' },
+          // cameraId,
           config,
-          (decodedText, decodedResult) => {
-            onScanSuccess(decodedText, decodedResult);
-          },
-          errorMessage => {
-            // ignore error
-          }
+          onScanSuccess
+          // onScanFailure
         );
       }
     })
     .catch(err => {
-      console.error(err);
+      console.error('QR getCameras failed: ' + err);
     });
 };
 const stopQrScanner = () => {
   html5QrScanner.value
     .stop()
     .then(ignore => {
-      console.log('QR Scanning stopped');
+      console.log('QR stopped');
     })
     .catch(err => {
-      console.error(err);
+      console.error('QR stop failed: ' + err);
     });
 };
 
@@ -63,10 +57,82 @@ onMounted(() => {
 onUnmounted(() => {
   stopQrScanner();
 });
+
+const invertedCanvas = ref(null);
+
+// !EXPERIMENTAL
+function onScanFailure() {
+  const videoElement = document.querySelector(`#${elementName.value} video`);
+  if (videoElement) {
+    let canvasElement;
+    let canvasContext;
+    if (invertedCanvas.value) {
+      canvasElement = invertedCanvas.value;
+      canvasElement.width = videoElement.videoWidth;
+      canvasElement.height = videoElement.videoHeight;
+      canvasContext = canvasElement.getContext('2d');
+    } else {
+      canvasElement = document.createElement('canvas');
+      canvasElement.width = videoElement.videoWidth;
+      canvasElement.height = videoElement.videoHeight;
+      canvasContext = canvasElement.getContext('2d');
+    }
+
+    canvasContext.drawImage(
+      videoElement,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+
+    invertColors(canvasContext, canvasElement.width, canvasElement.height);
+
+    canvasElement.toBlob(blob => {
+      if (
+        html5QrScanner.value &&
+        html5QrScanner.value.getState() === 'SCANNING'
+      ) {
+        html5QrScanner.value.stop();
+      }
+
+      html5QrScanner.value
+        .scanFile(blob, true)
+        .then(decodedText => {
+          onScanSuccess(decodedText);
+        })
+        .catch(err => {
+          html5QrScanner.value.start(
+            cameraId,
+            config,
+            onScanSuccess,
+            onScanFailure
+          );
+          console.warn('QR scanFile failed: ' + err);
+        });
+    }, 'image/png');
+  }
+}
+
+function invertColors(context, width, height) {
+  const imageData = context.getImageData(0, 0, width, height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i]; // Red
+    data[i + 1] = 255 - data[i + 1]; // Green
+    data[i + 2] = 255 - data[i + 2]; // Blue
+  }
+
+  context.putImageData(imageData, 0, 0);
+}
 </script>
 
 <template>
-  <div :id="elementName" class="h-full w-full" />
+  <div>
+    <div :id="elementName" class="h-[50%] w-full" />
+    <canvas ref="invertedCanvas" class="h-[50%] w-full" />
+  </div>
 </template>
 
 <style></style>
